dbae213fbb9e35e4d312a9a47465328fb952d689
Malyuchenko Maxim <163737524+mak-malo@users.noreply.github.com>
2025-06-02 20:37:56 +0300

    Update Kursovaya.ino

--- Kursovaya/Kursovaya.ino
+++ Kursovaya/Kursovaya.ino
@@ -1,9 +1,10 @@
 const int analogPin = A0;       // Аналоговый пин для потенциометра
-const int pwmLedPin = 6;        // Цифровой пин с ШИМ для светодиода
+const int pwmLedPin = 9;        // Цифровой пин с ШИМ для светодиода
 
-bool isSender = false;          // По умолчанию каждый МК сначала Приемник
+bool isSender = true;          // По умолчанию каждый МК сначала Приемник
 int previousValue = -1;
 int receivedValue = 0;
+const int buttonSlave = 7;
 
 void setup() {
   pinMode(analogPin, INPUT);
@@ -12,14 +13,17 @@ void setup() {
 
   Serial.begin(9600);
   delay(1000); // Ожидание стабильности UART
-
-  // Определение роли: если сигнал на A0 > 10, значит подключён потенциометр, а МК выполняет роль Отправителя
-  if (analogRead(analogPin) > 10) {
-    isSender = true;
-  }
 }
 
 void loop() {
+
+  if (digitalRead(buttonSlave) == LOW) {
+    delay(30);
+    if (digitalRead(buttonSlave) == LOW) {
+      isSender = false;
+    }
+  }
+
   if (isSender) {                                 // Если МК Отправитель
     int analogValue = analogRead(analogPin);      // Считывание аналогового сигнала
     int valueToSend = analogValue / 4;            // Преобразуем 0–1023 в 0–255
@@ -28,10 +32,12 @@ void loop() {
       previousValue = valueToSend;
       delayMicroseconds(100);                     // Задержка 10 мс необходима для сглаживания колебаний яркости светодиода
     }
-  } else {                                        // Если МК Приемник
+  }
+   else {                                        // Если МК Приемник
     if (Serial.available()) {
       receivedValue = Serial.parseInt();
-      analogWrite(pwmLedPin, constrain(receivedValue, 0, 255));
+      analogWrite(pwmLedPin, receivedValue / 4);
+
     }
   }
 }
